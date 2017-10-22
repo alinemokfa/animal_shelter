8b3ea9db90fd82a20f0590b4b93a5d4b82b8f4f6
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-22 23:25:19 +0100

    added animal_2 + animal_2.save

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,19 @@ animal_1 = Animal.new({
   'image_url' => '../public/images/animals/dogs/arf_vader.jpeg',
   'adopted' => 'false'
   })
-  
+
 animal_1.save()
+
+animal_2 = Animal.new({
+  'name' => 'Chew Barka',
+  'type' => 'dog',
+  'breed' => 'Lhasa Apso',
+  'adoption_status' => 'false',
+  'image_url' => '../public/images/animals/dogs/chew_barka.jpeg',
+  'adopted' => 'false'
+  })
+
+animal_2.save()
 
 binding.pry
 nil
