8c0006f6d26e2b193dbb27faa82452ab9ab0f6f0
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-22 18:33:52 +0100

    corrected typos - added comma to end of line 8 + binding.pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ require_relative( '../models/animal.rb' )
 animal_1 = Animal.new({
   'name' => 'Arf Vader',
   'type' => 'dog',
-  'breed' => 'French Bulldog'
+  'breed' => 'French Bulldog',
   'adoption_status' => 'false',
   'image_url' => '../public/images/animals/dogs/arf_vader.jpeg',
   'adopted' => 'false'
   })
 
-binding.binding.pry
+binding.pry
 nil
