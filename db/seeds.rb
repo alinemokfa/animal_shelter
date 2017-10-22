360ca9301106c81b30fee8ea31e08d7edfc04ba6
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-22 18:07:04 +0100

    added animal_1

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require_relative('../models/animal.rb')
+
+animal_1 = Animal.new({
+  'name' => 'Arf Vader',
+  'type' => 'dog',
+  'breed' => 'French Bulldog'
+  'adoption_status' => 'false',
+  'image_url' => '../public/images/animals/dogs/arf_vader.jpeg',
+  'adopted' => 'false'
+  })
