755cf312ca93c57cf5d107e18e06ba6e0560be88
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-23 13:58:42 +0100

    created get /

---
+++ app.rb
@@ -0,0 +1,9 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require_relative('controllers/animals_controller.rb')
+# require_relative('controllers/owners_controller')
+# require_relative('controllers/adoptions_controller')
+
+get '/' do
+  erb( :index )
+end
