9be62a2eb52c89503722d4b59af38d1541c71334
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-26 10:47:23 +0100

    added req_rel for adoptions_controller

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/animals_controller.rb')
-require_relative('controllers/owners_controller')
+require_relative('controllers/owners_controller.rb')
+require_relative('controllers/adoptions_controller.rb')
 
 get '/' do
   erb( :home )
