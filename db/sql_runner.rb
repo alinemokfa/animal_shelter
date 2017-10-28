2a8aefa8f44034623263a13cea02bea9cc1b9ac5
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-28 15:31:07 +0100

    config file created to protect db password

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,10 +1,12 @@
 require( 'pg' )
+require_relative('./config')
 
 class SqlRunner
 
   def self.run( sql, values )
     begin
-      db = PG.connect({ dbname: 'animal_shelter', host: 'localhost' })
+      # db = PG.connect({ dbname: 'animal_shelter', host: 'localhost' })
+      db = PG.connect($db)
       db.prepare( "query", sql )
       result = db.exec_prepared( "query", values )
     ensure
