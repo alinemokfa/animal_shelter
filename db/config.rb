8c392e7ee02fa062e2f8cc26e2232ea3e3a0037e
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-30 16:01:16 +0000

    rotated heroku credentials and added heroku ENV variables

--- db/config.rb
+++ db/config.rb
@@ -1,5 +1,1 @@
-$db = {dbname: ENV['DBNAME'],
-  host: ENV['HOST'],
-  port: ENV['PORT'],
-  user: ENV['USER'],
-  password: ENV['PASSWORD'] }
+$db = 
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,7 +5,11 @@ class SqlRunner
   def self.run( sql, values )
     begin
       # db = PG.connect({ dbname: 'animal_shelter', host: 'localhost' })
-      db = PG.connect($db)
+      db = PG.connect({dbname: ENV['DBNAME'],
+        host: ENV['HOST'],
+        port: ENV['PORT'],
+        user: ENV['USER'],
+        password: ENV['PASSWORD']})
       db.prepare( "query", sql )
       result = db.exec_prepared( "query", values )
     ensure
