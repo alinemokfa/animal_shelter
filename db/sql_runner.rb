347cebe3ad1803715b0bf2cc2a9698b0ba438d36
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-30 08:36:04 +0000

    protected password

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -5,11 +5,7 @@ class SqlRunner
   def self.run( sql, values )
     begin
       # db = PG.connect({ dbname: 'animal_shelter', host: 'localhost' })
-      db = PG.connect({dbname: 'd8hnihh8g2jlh6',
-        host: 'ec2-184-72-248-8.compute-1.amazonaws.com',
-        port: 5432,
-        user: 'yxdayxjhbkqldm',
-        password: '8dd5b95b9f387d803e2e21674fb25bf749f5d8f01d9d016144140c945d530140' })
+      db = PG.connect($db)
       db.prepare( "query", sql )
       result = db.exec_prepared( "query", values )
     ensure
